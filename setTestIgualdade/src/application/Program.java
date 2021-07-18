608eb58db87d7f2ae7577d72cd9c1b3917dc5c6f
Celio Iyama <celio.iyama@gmail.com>
2021-07-17 22:40:31 -0300

    Set testa igualdade com os metodos hashCode e equals- Java

---
+++ setTestIgualdade/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Set<Product> set = new HashSet<>();
+		
+		set.add(new Product("Tv", 900.0));
+		set.add(new Product("Notebook", 1200.0));
+		set.add(new Product("Tablet", 400.0));
+		
+		Product prod = new Product("Notebook", 1200.0);
+		
+		System.out.println(set);
+		System.out.println(prod);
+		System.out.println();
+		
+		System.out.println(set.contains(prod));
+		
+	}
+
+}
