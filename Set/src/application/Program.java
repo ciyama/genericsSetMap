42efcf42c6883638802dcd865116e6669ee98449
Celio Iyama <celio.iyama@gmail.com>
2021-07-17 21:50:32 -0300

    TreeSet - Java

--- Set/src/application/Program.java
+++ Set/src/application/Program.java
@@ -1,16 +1,16 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<String> set = new HashSet<>();
+		Set<String> set = new TreeSet<>();
 		
 		set.add("Tablet");
-		set.add("TV");
+		set.add("Tv");
 		set.add("Notebook");
 		
 		System.out.println(set.contains("Notebook"));
