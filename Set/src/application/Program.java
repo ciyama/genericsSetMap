f70b251b6364635960bf54c4451541dd9b4927aa
Celio Iyama <celio.iyama@gmail.com>
2021-07-17 21:57:17 -0300

    Set remove, removeIf - Java

--- Set/src/application/Program.java
+++ Set/src/application/Program.java
@@ -1,19 +1,22 @@
 package application;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<String> set = new TreeSet<>();
+		Set<String> set = new LinkedHashSet<>();
 		
 		set.add("Tablet");
 		set.add("Tv");
 		set.add("Notebook");
 		
-		System.out.println(set.contains("Notebook"));
+		//set.remove("Tv");
+		//set.removeIf(x -> x.length() > 3);
+		set.removeIf(x -> x.charAt(0) == 'T' );
+		
 		
 		for (String p : set) {
 			System.out.println(p);
